36ef57bb41080b365d51666c0118bc138e0e1b0c
shahd-s <ssherif96@aucegypt.edu>
2020-11-20 22:06:06 +0200

    Uploading

---
+++ Assets/L_System.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class L_System : MonoBehaviour
+{
+    public LineRenderer tree;//= new LineRenderer();
+    public List<Vector3> verts = new List<Vector3>();
+    public string axiom;
+    public bool yes = true;
+    public bool probability = false;
+    public float delay = 0.1f;
+    private string initaxiom;
+
+    [Serializable]
+    public struct Rule
+    {
+        public string name;
+        public string rule;
+    }
+    public Rule[] rules;
+
+    private int programindex = 0;
+    private Dictionary<string, string> prodRules = new Dictionary<string, string>();
+    private double toRad = Math.PI / 180.0;
+    public double angle = 25.7;
+    private double alpha = 90;
+    public int n = 5;
+    public double d = 1;
+
+    private struct StackElement
+    {
+        public int ind;
+        public double ang;
+        public StackElement(int x, double y)
+        {
+            this.ind = x;
+            this.ang = y;
+        }
+    }
+
+    private Stack<StackElement> stack = new Stack<StackElement>();
+    // Start is called before the first frame update
+
+      public IEnumerator
+    
+     FillVertsStepWise(int nn, float delay)
+    {
+        for (  int i = 0; i < nn; i++)
+        {
+            for (int j = 0; j < axiom.Length; j++)
+            {
+                switch (axiom[j])
+                {
+                    case 'F':
+                        verts.Add(new Vector3((float)(verts[verts.Count - 1].x + d * Math.Cos(alpha * toRad)), (float)(verts[verts.Count - 1].y + d * Math.Sin(alpha * toRad)), 0));
+                       yield return new WaitForSeconds(delay);
+                        break;
+                    case '+':
+                        alpha += angle;
+                        break;
+                    case '-':
+                        alpha -= angle;
+                        break;
+                    case '[':
+                        stack.Push(new StackElement(verts.Count - 1, alpha));
+                        break;
+                    case ']':
+                        StackElement t = stack.Pop();
+                        alpha = t.ang;
+                        for (int x = verts.Count - 1; x >= t.ind; x--)
+                             verts.Add(verts[x]);
+                        break;
+
+                }
+                int index = verts.Count - 1;
+               // Debug.Log("Done with: " + axiom[j] + "Angle is: " + alpha.ToString() + " and pos is: " + verts[index].x + ", " + verts[index].y + " index is: " + index.ToString());
+                // yield return new WaitForSeconds(0f);
+
+
+            }
+
+           // yield return new WaitForSeconds(0.5f);
+            //replace ax
+            // axiom=axiom.Replace()
+           // Debug.Log(i.ToString() + " ax is: " + axiom);
+
+            //for (int t=0; t<axiom.Length; t++)
+            //{
+            //    Debug.Log("rep" + axiom[t]);
+            //    if(prodRules.ContainsKey (axiom[t].ToString()))
+            //    axiom = axiom.Replace(axiom[t].ToString(), prodRules[axiom[t].ToString()]);
+            //}
+            
+        }
+    }
+
+
+    void
+    FillVerts(int nn)
+    {
+        for (int i = 0; i < nn; i++)
+        {
+            for (int j = 0; j < axiom.Length; j++)
+            {
+                switch (axiom[j])
+                {
+                    case 'F':
+                        verts.Add(new Vector3((float)(verts[verts.Count - 1].x + d * Math.Cos(alpha * toRad)),
+                            (float)(verts[verts.Count - 1].y + d * Math.Sin(alpha * toRad)),
+                            0));
+                        break;
+                    case '+':
+                        alpha += angle;
+                        break;
+                    case '-':
+                        alpha -= angle;
+                        break;
+                    case '[':
+                        stack.Push(new StackElement(verts.Count - 1, alpha));
+                        break;
+                    case ']':
+                        StackElement t = stack.Pop();
+                        alpha = t.ang;
+                        for (int x = verts.Count - 1; x >= t.ind; x--)
+                            verts.Add(verts[x]);
+                        break;
+
+                }
+
+                int index = verts.Count - 1;  
+            }
+             
+        }
+    }
+
+    void Start()
+    {
+        Debug.Log("probability is " + probability);
+        initaxiom = axiom;
+        tree.startColor = Color.white;
+        tree.endColor = Color.white;
+       
+        foreach (Rule r in rules)
+        {
+            if(!prodRules.ContainsKey(r.name))
+            prodRules.Add(r.name, r.rule);
+        }
+
+        verts.Add(new Vector3(0, 0, 0));
+
+        if(!probability)
+        for (int i = 0; i < n; i++)
+        {
+
+            foreach (string key in prodRules.Keys)
+            {
+                axiom = axiom.Replace(key, prodRules[key]);
+            }
+
+
+
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                System.Random rand = new System.Random();
+                int index = rand.Next(rules.Length);
+                axiom = axiom.Replace(rules[index].name, rules[index].rule);
+                 
+            }
+        }
+        if (yes)
+            FillVerts(1);
+        else
+            StartCoroutine(FillVertsStepWise(1, delay));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+         
+
+        tree.positionCount = verts.Count;
+        tree.SetPositions(verts.ToArray());
+
+
+    }
+}
---
+++ Assets/controller.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    [Serializable]
+    public struct LSystem
+    {
+        
+        public string axiom;
+        public L_System.Rule[] rules;
+        public double angle;
+        public double d;
+        public int n;
+    }
+
+    public LSystem[] lsystems;
+    private LSystem[] lSystemsinit;
+
+    private bool toggle = true;
+
+    private bool probability = false;
+    [Range(0.0001f, 0.1f)]
+    public float delay = 0.01f;
+
+
+    private GameObject g;
+    private GameObject T;
+    public float lengthStep = 0.005f;
+    public int LSysIndex=0;
+    public int startingit = 0;
+    public float angleStep = 5;
+
+    void Start()
+    {
+        g = Instantiate((GameObject)Resources.Load("Prefab/LSys"));
+        T = Instantiate((GameObject)Resources.Load("Prefab/Txt"));
+
+        g.GetComponent<L_System>().n = startingit;
+        g.GetComponent<L_System>().axiom = lsystems[LSysIndex].axiom;
+        g.GetComponent<L_System>().rules = lsystems[LSysIndex].rules;
+        g.GetComponent<L_System>().angle = lsystems[LSysIndex].angle;
+        g.GetComponent<L_System>().d = lsystems[LSysIndex].d;
+        lSystemsinit = new List<LSystem>(lsystems).ToArray();  
+
+    }
+    void instan()
+    {
+        Destroy(g);
+        g = Instantiate((GameObject)Resources.Load("Prefab/LSys"));
+        g.GetComponent<L_System>().probability = probability;
+        g.GetComponent<L_System>().n = startingit;
+        g.GetComponent<L_System>().axiom = lsystems[LSysIndex].axiom;
+        g.GetComponent<L_System>().rules = lsystems[LSysIndex].rules;
+        g.GetComponent<L_System>().angle = lsystems[LSysIndex].angle;
+        g.GetComponent<L_System>().d = lsystems[LSysIndex].d;
+    }
+    void Update()
+    {
+        probability = (LSysIndex == 8);
+        //incriment iteration
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (startingit + 1 <= lsystems[LSysIndex].n)
+            {
+                startingit++;
+                instan();
+            }
+        }
+        else
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            
+            if (startingit - 1 >= 0)
+                {
+                    startingit--;
+                     instan();
+                   
+                }
+
+            
+        }
+        else
+            if(Input.GetKeyDown(KeyCode.C))
+        {
+            if(lsystems[LSysIndex].d -lengthStep >= 0)
+            {
+                lsystems[LSysIndex].d -= lengthStep;
+                instan();
+            }
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.D))
+        {
+
+            lsystems[LSysIndex].d += lengthStep;
+                instan();
+            
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            lsystems[LSysIndex].n++;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (lsystems[LSysIndex].n - 1 >= 0)
+            {
+                lsystems[LSysIndex].n--;
+                startingit--;
+            }
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            lsystems[LSysIndex].angle += angleStep;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+            lsystems[LSysIndex].angle -= angleStep;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+
+            LSysIndex = 0;
+            startingit = 0;
+            toggle = true;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            toggle = true;
+            LSysIndex = 1;
+            startingit = 0;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            toggle = true;
+            LSysIndex = 2;
+            startingit = 0;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            toggle = true;
+            LSysIndex = 3;
+            startingit = 0;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            toggle = true;
+            LSysIndex = 4;
+            startingit = 0;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            toggle = true;
+            LSysIndex = 5;
+            startingit = 0;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            toggle = true;
+            LSysIndex = 6;
+            startingit = 0;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            toggle = true;
+            LSysIndex = 7;
+            startingit = 0;
+            instan();
+
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            //probability one.
+            toggle = true;
+            LSysIndex = 8;
+            startingit = 0;
+            probability = true;
+            instan();
+
+            
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (toggle)
+         
+                startingit = lsystems[LSysIndex].n;
+
+           
+            else
+                startingit = 0;
+
+         
+            lsystems = new List<LSystem>(lSystemsinit).ToArray();
+            instan();
+            toggle = !toggle;
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.S))
+        {
+            Destroy(g);
+            g = Instantiate((GameObject)Resources.Load("Prefab/LSys"));
+
+            g.GetComponent<L_System>().n = lsystems[LSysIndex].n;
+            g.GetComponent<L_System>().axiom = lsystems[LSysIndex].axiom;
+            g.GetComponent<L_System>().rules = lsystems[LSysIndex].rules;
+            g.GetComponent<L_System>().angle = lsystems[LSysIndex].angle;
+            g.GetComponent<L_System>().d = lsystems[LSysIndex].d;
+            g.GetComponent<L_System>().yes = false;
+            g.GetComponent<L_System>().delay = delay ;
+        }
+        string p0 = "DISPLAYING L-SYSTEM " + (LSysIndex + 1).ToString() + '\n' + '\n';
+        string p1= "n=" + lsystems[LSysIndex].n.ToString() + ",δ=" + lsystems[LSysIndex].angle.ToString()
+            + '\n' + lsystems[LSysIndex].axiom + '\n';
+
+        string p2 = "";
+        for (int i = 0; i < lsystems[LSysIndex].rules.Length; i++)
+        {
+            p2 += lsystems[LSysIndex].rules[i].name + "→" + lsystems[LSysIndex].rules[i].rule + '\n';
+        }
+        p2 += "d= " + lsystems[LSysIndex].d.ToString()+'\n';
+        p2 += "i= " + startingit.ToString();
+        T.GetComponent<TMP_Text>().text =p0+ p1 + p2;
+
+    }
+}
